d556fa704a48e5d5ccd9c8b22d5da0282bf54aa3
unknown-user <unknown-user@192.168.2.7>
2012-05-30 11:50:11 +0100

    Brought in local copy of StronglyTypedResourceBuilder.

--- StronglyTypedResourceBuilderTests/StronglyTypedResourceBuilderOtherTests.cs
+++ StronglyTypedResourceBuilderTests/StronglyTypedResourceBuilderOtherTests.cs
@@ -11,19 +11,6 @@ using System.Drawing;
 namespace StronglyTypedResourceBuilderTests {
 	[TestFixture]
 	public class StronglyTypedResourceBuilderOtherTests	{
-		static string [] keywords = {"abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", 
-									"checked", "class", "const", "continue", "decimal", "default", "delegate", 
-									"do", "double", "else", "enum", "event", "explicit", "extern", "FALSE", 
-									"false", "finally", "fixed", "float", "for", "foreach", "goto", "if", 
-									"implicit", "in", "int", "interface", "internal", "is", "lock", "long", 
-									"namespace", "new", "null", "object", "operator", "out", "override", "params", 
-									"private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", 
-									"short", "sizeof", "stackalloc", "static", "string", "struct", "switch", "this", 
-									"throw", "TRUE", "true", "try", "typeof", "uint", "ulong", "unchecked", "unsafe", 
-									"ushort", "using", "virtual", "volatile", "void", "while" };
-		static char [] specialChars = { ' ', '\u00A0', '.', ',', ';', '|', '~', '@', '#', '%', '^', '&', 
-										'*', '+', '-', '/', '\\', '<', '>', '?', '[', ']', '(', ')', '{', 
-										'}', '\"', '\'', ':', '!'};
 		CSharpCodeProvider provider = new CSharpCodeProvider ();
 		
 		Dictionary<string,object> GetAllResourceTypes ()
